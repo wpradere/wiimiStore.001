68fd23fc794657c8ad003146fce0538ae5e0d8a8
WilliKahan <skaoi39@gmail.com>
2025-02-05 14:49:43 -0500

    se modifica .sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
+# Temporizador: 2 minutos (120 segundos)
+TIMEOUT=120
+START_TIME=$(date +%s)
+
 echo "📌 Esperando a que PostgreSQL esté listo..."
-until nc -z -v -w30 postgres 5432; do
+
+# Revisa si PostgreSQL está disponible dentro del tiempo límite
+until PGPASSWORD=$POSTGRES_PASSWORD pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER; do
+  CURRENT_TIME=$(date +%s)
+  ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
+
+  # Si han pasado más de 120 segundos, salimos
+  if [ $ELAPSED_TIME -ge $TIMEOUT ]; then
+    echo "❌ Tiempo de espera agotado. PostgreSQL no está listo."
+    exit 1
+  fi
+
   echo "⏳ Esperando PostgreSQL..."
   sleep 1
 done
+
 echo "✅ PostgreSQL está listo!"
 
 # Ejecuta las migraciones de Prisma
